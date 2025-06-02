edbbad59082e4957c6a3a2ff4ab9190dbedcb6b2
Mia Bosheva <boshevam@gmail.com>
2025-06-02 21:21:07 +0200

    Initial Commit

---
+++ HarryPotter/HarryPotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterApp.swift
+//  HarryPotter
+//
+//  Created by Mia on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
